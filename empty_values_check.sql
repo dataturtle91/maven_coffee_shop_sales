d5579b9e4a40986488fa646ad8ab9b013998f3b3
dataturtle <150851332+dataturtle91@users.noreply.github.com>
2026-02-14 18:25:01 +0100

    Implement empty value checks for sales fields
    Added checks for empty values in various sales transaction fields.

--- empty_values_check.sql
+++ empty_values_check.sql
@@ -1,9 +1,76 @@
 -- Maven Coffee Shop Sales
 -- Store location empty values check
+-- Maven Coffee Shop Sales
+-- Table contains 149 116 rows
+-- Empty value check
+
 SELECT
-	'store_location' AS _variable
-	,COUNT(DISTINCT store_location) AS _unique
-	,(SELECT COUNT(store_location) FROM sales WHERE store_location IS NULL) AS _null
+	'transaction_id' AS _variable
+	,COUNT(DISTINCT transaction_id) AS _unique
+	,(SELECT COUNT(transaction_id) FROM sales WHERE transaction_id IS NULL) AS _null
+	
+FROM sales
+
+UNION ALL 
+
+SELECT
+	'transaction_date' 
+	,COUNT(DISTINCT transaction_date)
+	,(SELECT COUNT(transaction_date) FROM sales WHERE transaction_date IS NULL)
+	
+FROM sales
+
+UNION ALL 
+
+SELECT
+	'transaction_time' 
+	,COUNT(DISTINCT transaction_time) 
+	,(SELECT COUNT(transaction_time) FROM sales WHERE transaction_time IS NULL) 
+	
+FROM sales
+
+UNION ALL 
+
+SELECT
+	'transaction_qty' 
+	,COUNT(DISTINCT transaction_qty)
+	,(SELECT COUNT(transaction_qty) FROM sales WHERE transaction_qty IS NULL)
+	
+FROM sales
+
+UNION ALL 
+
+SELECT
+	'store_id' 
+	,COUNT(DISTINCT store_id) 
+	,(SELECT COUNT(store_id) FROM sales WHERE store_id IS NULL) 
+	
+FROM sales
+
+UNION ALL 
+
+SELECT
+	'store_location'
+	,COUNT(DISTINCT store_location)
+	,(SELECT COUNT(store_location) FROM sales WHERE store_location IS NULL)
+	
+FROM sales
+
+UNION ALL 
+
+SELECT
+	'product_id'
+	,COUNT(DISTINCT product_id)
+	,(SELECT COUNT(product_id) FROM sales WHERE product_id IS NULL)
+	
+FROM sales
+
+UNION ALL 
+
+SELECT
+	'unit_price'
+	,COUNT(DISTINCT unit_price)
+	,(SELECT COUNT(unit_price) FROM sales WHERE unit_price IS NULL)
 	
 FROM sales
 
@@ -36,4 +103,6 @@ FROM
 	sales;
 
 
--- The variables store_location, product_category, product_type and product_detail do not contain empty values
+
+-- All variables do not contain empty values. 
+-- Variables that should contain unique values ​​do not contain duplicates
